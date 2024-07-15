7a436a149cf37711c1481abe62c9ba882a0d3ec9
Antônio Enzo Bezerra / An1Toh <159924839+antonioenzobz@users.noreply.github.com>
2024-07-15 11:16:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 // Menu mobile
+function expand(){
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('mobile');
+}
 
 // Imagem 1
 const imagem1 = document.getElementById("lampada-01");
@@ -12,3 +16,11 @@ imagem1.addEventListener("click", function () {
 });
 
 // Imagem 2
+const imagem2 = document.getElementById("lampada-02");
+imagem2.addEventListener("mouseover", function () {
+    imagem2.src = "img/lampada-on.jpg";
+});
+
+imagem2.addEventListener("mouseout", function () {
+  imagem2.src = "img/lampada.jpg";
+});
